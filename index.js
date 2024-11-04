92543d4e6b396cdf6ea5f8d7166f2d40765ba563
rock's ezweb <alikerock@gmail.com>
2024-11-04 15:08:31 +0900

    insert 구현

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const express = require('express')
 const app = express()
 const port = 8000
 
+//본문을 통해서 넘어온 요청 파싱(변환) 미들웨어(body-parser)
+//본문을 통해서 넘어온 name=Alice&age=25
+app.use(express.json()); //json 형식으로 변환 { "name":"Alice", "age":"25"}
+app.use(express.urlencoded()); //json->object { name: "Alice", age: "25" } 
+
 var corsOptions = {
   //origin: 'http://localhost:3000/',
   origin: '*' //모든 출처 허용
@@ -34,6 +39,17 @@ app.get('/', (req, res) => {
 app.get('/list', (req, res) => {
   const sql = "SELECT BOARD_ID, BOARD_TITLE, REGISTER_ID, DATE_FORMAT(REGISTER_DATE , '%Y-%m-%d') AS REGISTER_DATE FROM board";
   db.query(sql, (err, result) => {
+    if (err) throw err;  
+    res.send(result);
+  })  
+})
+
+app.post('/insert', (req, res) => {
+
+  let title = req.body.title;
+  let content = req.body.content;
+  const sql = "INSERT INTO board (BOARD_TITLE, BOARD_CONTENT, REGISTER_ID) VALUES (?,?,'admin')";
+  db.query(sql, [title, content], (err, result) => {
     if (err) throw err;  
     res.send(result);
   })  
